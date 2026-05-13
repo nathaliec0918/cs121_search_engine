e2e595bfacfe737a7f043582572f4c151ff51093
Nathalie Chiu <nathaliec0918@gmail.com>
2026-05-13 00:11:17 -0700

    Add indexer.py
    Co-authored-by: Thy Tran <trannathy@users.noreply.github.com>
    Co-authored-by: andrewdnguyen17 <andrewdnguyen17@users.noreply.github.com>

--- indexer.py
+++ indexer.py
@@ -1,5 +1,11 @@
 import json
 import sys
+import os.path
+from pathlib import Path
+import re
+from bs4 import BeautifulSoup
+from nltk.stem import PorterStemmer
+
 
 '''
 Posting structure {
@@ -18,50 +24,97 @@ Report structure (json) {
 }
 '''
 
+STEMMER = PorterStemmer()
+
+
 class Posting:
-    def __init__(self, docID, freq_count, fields):
+    def __init__(self, docID, freq_count, fields = None, positions = None):
         self.docID = docID
         self.freq_count = freq_count
-        self.fields = fields
+        #self.fields = fields for future assignments
+        #self.positions = positions
 
-# general flow
-def build_index(corpus:set):
+# partial index flow 
+def build_index(frontier: list):
     # basic flow
     index = dict()
     docID = 0
-    for document in corpus:
-        docID += 1
-        parsed_doc = parser(document)
-        token = tokenize(parsed_doc)
-        unique_token = set(list(token)) # remove duplicates
-        for t in unique_token:
-            if index[t] not in index:
-                index[t] = []
-            index[t].append(Posting(docID))
-    
+    batch = []
+    while frontier:
+        batch = get_batch(frontier, 100)
+        for document in batch:
+            docID += 1
+            stemmed_tokens = parse_to_token(document)
+            unique_token = set(stemmed_tokens) # remove duplicates
+            for t in unique_token:
+                if t not in index:
+                    index[t] = []
+                index[t].append(Posting(docID))
+        sort_and_write_to_disk(index, name) # uploading too Disk, reset RAM
+        index = dict()
+        
     return index
 
-def parser(document:json) -> list:
-    with open(document, "r", encoding="utf-8") as file:
-        try:
-            parsed = json.load(file)
-        except json.decoder.JSONDecodeError:
-            parsed = {
-                "num_indexed_doc": 0,
-                "num_unique_tokens": 0,
-                "index_size": 0,
-                "indexed_doc": {"docID": 0, "token": ""},
-                "unique_tokens": dict()
-            }
-    return parsed 
+def get_batch(frontier: list, batch_size: int)->list :
+    #returns batch of first n urls and removes them from the frontier
+    batch = frontier[:batch_size]
+    frontier = frontier[batch_size:]
+    return batch
+
+
+def sort_and_write_to_disk(index, name):
+    ...
+
+def parse_to_token(document: json) -> list[str]:
+    try: 
+        with open(document, "r", encoding="utf-8") as f:
+            document = json.load(f)
+    except (json.JSONDecodeError, OSError):
+        ...
+    # try: 
+    #     soup = BeautifulSoup(content, "lxml")
+    # except:
+    #     soup = BeautifulSoup(content, "html.parser")
+
+
+# def report(document:json) -> list:
+#     with open(document, "r", encoding="utf-8") as file:
+#         try:
+#             parsed = json.load(file)
+#         except json.decoder.JSONDecodeError:
+#             parsed = {
+#                 "num_indexed_doc": 0,
+#                 "num_unique_tokens": 0,
+#                 "index_size": 0,
+#                 "indexed_doc": {"docID": 0, "token": ""},
+#                 "unique_tokens": dict()
+#             }
+#     return parsed
+
+def get_corpus(top, corpus: list):
+    for dirpath, dirnames, filenames in os.walk(top):
+        for name in filenames:
+            corpus.append(os.path.join(dirpath, name))
 
 def tokenize(parsed_doc):
-    # nltk
-    pass    
+    # beautiful soup
+    # json: {"url":"", "content":""}
+    
+    pass   
+
+def main():
+    if len(sys.argv) == 2:
+        filePath = sys.argv[1]
+        try:
+            corpus = []
+            get_corpus(filePath, corpus)
+            build_index(corpus)
+        
+        except:
+            print("Unable to open path or path is invalid. Please restart the program to try again.") # CHANGE LATER
+    else:
+        print("Please restart the program and specify one file path") 
 
 
 if __name__ == "__main__":
-
-    pass
-
-
+    main()
