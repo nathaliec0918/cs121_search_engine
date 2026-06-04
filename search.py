c6b2e68fcd6fb1d094e4c61fb2761f6f2328348b
Nathalie Chiu <nathaliec0918@gmail.com>
2026-06-03 23:39:44 -0700

    Implemented word positioning

--- search.py
+++ search.py
@@ -54,30 +54,79 @@ def calc_tfidf(tf: int, df: int, n: int, imp_freq: int = 0) -> float:
 def get_intersection(search_postings_dict: dict, total_n) -> dict:
     if not search_postings_dict:
         return {}
+    
+    # structure (token: posting list), (token2: posting list)
     sorted_search_postings_dict = dict(sorted(search_postings_dict.items(), key=lambda x: len(x[1])))
-    # get only shortest p list to compare
-    first_token = next(iter(sorted_search_postings_dict))
+    
+    # Posting[positions] have all the position of the that token
+    # want positions_docs: {doc_id1: {token1: [pos1, pos2], token2: [pos1]}, doc_id2:...}
+
+    # start only with the first posting (shortest length=rarest word -> token appears in least amount of docs) 
+    first_token = next(iter(sorted_search_postings_dict)) # rarest token
     intersection_docs = {} # INITIALIZED - NATH
-    for p in sorted_search_postings_dict[first_token]:
+    positions_doc = {}
+    for p in sorted_search_postings_dict[first_token]: # looping thru all postings for rarest token (set of doc_id)
         intersection_docs[p["doc_ID"]] = calc_tfidf(
             p["freq"], len(sorted_search_postings_dict[first_token]), total_n, p["imp_freq"]
-        ) # len(sorted_search_postings_dict[first_token]) instead of len(p) - NATH
+        ) # len(sorted_search_postings_dict[first_posting]) instead of len(p) - NATH
+        
+        if p["positions"]: # check if token has positions in that doc_id
+            positions_doc[p["doc_ID"]] = {first_token: p["positions"]}        
     
-    # boolean and
+    # boolean and with remaining token (except the first one)
     for token, p_list in list(sorted_search_postings_dict.items())[1:]:
         
         current = dict()
         for p in p_list:
-            current[p["doc_ID"]] = calc_tfidf(p["freq"], len(p_list), total_n, p["imp_freq"]) # len(p_list) instead len(p) - NATH
-        
+            #current[p["doc_ID"]] = calc_tfidf(p["freq"], len(p_list), total_n, p["imp_freq"]) # len(p_list) instead len(p) - NATH
+            current[p["doc_ID"]] = p # store the current posting
         update_intersection = dict()
+        
         for docID in intersection_docs:
             if docID in current:
-                update_intersection[docID] = current[docID] + intersection_docs[docID]
+                p = current[docID]
+                update_intersection[docID] = calc_tfidf(p["freq"], len(p_list), total_n, p["imp_freq"]) + intersection_docs[docID]
+                
+                # load positional_doc
+                if p["positions"] and docID in positions_doc:
+                    positions_doc[docID][token] = p["positions"]
+                    
         intersection_docs = update_intersection
+        # print(positions_doc) # contain positions of token in doc_id -> {11702: {'crista': [473], 'lope': [474]}, 11707: {'crista': [457, 496], 'lope': [458, 497, 533, 618]}, ...}
+        # print(intersection_docs) # contains relevance scores of doc_id -> {11702: 4.939824580444155, 11707: 7.124329481906486, ...}
+        # add positional boost to the score here
+        for docID in intersection_docs:
+           if docID in positions_doc:
+              intersection_docs[docID] += calc_positional_boost(positions_doc[docID])
+        #     #   print(calc_positional_boost(positions_doc[docID]))
     
     return intersection_docs
-   
+  
+  
+def calc_positional_boost(positions_of_tokens: dict) -> float:
+    boost = 0.0
+    all_tokens = list(positions_of_tokens.keys())
+    n = len(all_tokens) # length of the query tokens
+    consecutive_count = 0
+    if n < 2:
+        return 0.0 # single query
+    
+    beginning_pos = sorted(positions_of_tokens[all_tokens[0]])
+    # 1. right next to each other (within 1 space)
+    for begin in beginning_pos:
+        found = True
+        for i in range(1,n):
+            expected = begin + i
+            if expected not in positions_of_tokens[all_tokens[i]]:
+                found = False
+                break
+        if found:
+            consecutive_count += 1
+    boost += float(consecutive_count)
+    # if boost > 0: 
+    #     print(boost)
+    return boost * 5
+ 
 def rank_frequencies(intersection_docs: dict) -> list: # imp tf-idf here?
     if not intersection_docs:
         return []
